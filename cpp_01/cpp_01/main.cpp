e05dabea1b9b35978eb85dd016fd6f3d0a763d34
River7784 <119822951+River7784@users.noreply.github.com>
2024-02-08 21:01:08 +0900

    Add files via upload

---
+++ cpp_01/cpp_01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_01
+//
+//  Created by 유가람 on 2/8/24.
+//
+
+#include <iostream>
+
+int main()
+{
+    // 표준 출력 스트림으로 문장을 출력함
+    std::cout << "첫 c++ 프로그램" <<std::endl;
+    return 0;
+}
