9bcb04dc92a5b559adee5b89acff8ef65a308b09
River7784 <119822951+River7784@users.noreply.github.com>
2024-07-30 21:37:50 +0900

    Add files via upload

---
+++ cpp_33/cpp_33/Meter.h
@@ -0,0 +1,32 @@
+//
+//  Meter.h
+//  cpp_33
+//
+//  Created by 유가람 on 7/30/24.
+//
+
+#ifndef Meter_h
+#define Meter_h
+#include <iostream>
+using namespace std;
+
+class Meter {
+    int m;
+    int cm;
+    
+public:
+    //생성자
+    Meter() : m(0), cm(0) {}
+    Meter(int meter, int cmeter) : m(meter), cm(cmeter) {}
+    void display() const {
+        if(m)
+            cout << m << "m ";
+        if(cm|| !m)
+            cout << cm << "cm";
+        couy << endl;
+    }
+    int getM() const{return m;} //  데이터멤버 m 반환
+    int getCm() const{return cm;}   // 데이터 멤버 cm 반환
+};
+
+#endif /* Meter_h */
---
+++ cpp_33/cpp_33/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_33
+//
+//  Created by 유가람 on 7/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
