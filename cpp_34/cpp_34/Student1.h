1dda689f7b622e91b8d014a76016edc6b7b48acf
River7784 <119822951+River7784@users.noreply.github.com>
2024-08-03 12:54:07 +0900

    Add files via upload

---
+++ cpp_34/cpp_34/Student1.h
@@ -0,0 +1,27 @@
+//
+//  Student1.h
+//  cpp_34
+//
+//  Created by 유가람 on 8/3/24.
+//
+
+#ifndef Student1_h
+#define Student1_h
+#include <iostream>
+#include <string>
+#include "Person1.h"
+
+// Person을 상속받아서 선언
+class Student : Public Person {
+    string school;
+    
+public:
+    void setSchool(const string& s) { school = s; }
+    string getSchool() const { return school; }
+    void print() const {
+        Person::print();
+        cout << " goes to " << school;
+    }
+};
+
+#endif /* Student1_h */
