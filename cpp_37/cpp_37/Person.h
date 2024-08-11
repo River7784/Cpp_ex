c8cefc12da3058e2bc384e2476c47b957d605e5d
River7784 <119822951+River7784@users.noreply.github.com>
2024-08-11 21:13:25 +0900

    Add files via upload

---
+++ cpp_37/cpp_37/Person.h
@@ -0,0 +1,23 @@
+//
+//  Person.h
+//  cpp_37
+//
+//  Created by 유가람 on 8/11/24.
+//
+
+#ifndef Person_h
+#define Person_h
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+    string name;
+    
+public:
+    Person(const string& n) : name(n){}
+    string getName() const { return name }
+    void print() const { cout << name; }
+};
+
+#endif /* Person_h */
---
+++ cpp_37/cpp_37/main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  cpp_37
+//
+//  Created by 유가람 on 8/11/24.
+//
+
+#include <iostream>
+
